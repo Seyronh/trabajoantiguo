29573ef2f7c25c290f1f27b7ee14a74c0cc7ba11
Seyronh <seyronh@hotmail.com>
2023-04-16 20:49:14 +0200

    Test Barco

--- Code/core/src/com/mygdx/code/Barco.java
+++ Code/core/src/com/mygdx/code/Barco.java
@@ -1,28 +1,34 @@
 package com.mygdx.code;
 
 import com.badlogic.gdx.math.*;
-
+import com.badlogic.gdx.Gdx;
 public class Barco {
+	private float DegToRad = (float) Math.PI/180;
 	private Vector2 velocidad;
-	private Vector2 posicion;
-	private float vida;
-	private float cansancio = 100;
+	public Vector2 posicion;
+	public float vida;
+	public float cansancio = 100;
+	public float angulo = 90;
 	private TipoBarco elegido;
 	private float frenadanatural = -1;
 	
-	public Barco(TipoBarco elegido) {
+	public Barco(TipoBarco elegido,Vector2 posicion) {
 		this.elegido = elegido;
 		this.vida = this.elegido.vidamax;
+		this.posicion = posicion;
+		this.velocidad = new Vector2();
 	}
 	public void girarDerecha() {
-		this.velocidad.rotateDeg(this.elegido.movilidad*(this.cansancio/100));
+		this.velocidad.rotateDeg(-this.elegido.movilidad*(this.cansancio/100)*Gdx.graphics.getDeltaTime());
+		this.angulo -= this.elegido.movilidad*(this.cansancio/100)*Gdx.graphics.getDeltaTime();
 	}
 	public void girarIzquierda() {
-		this.velocidad.rotateDeg(-this.elegido.movilidad*(this.cansancio/100));
+		this.velocidad.rotateDeg(this.elegido.movilidad*(this.cansancio/100)*Gdx.graphics.getDeltaTime());
+		this.angulo += this.elegido.movilidad*(this.cansancio/100)*Gdx.graphics.getDeltaTime();
 	}
 	public void acelerar() {
 		Vector2 acel = new Vector2();
-		acel.set(this.elegido.aceleracion*(this.cansancio/100)*MathUtils.asin(this.velocidad.angleRad()), this.elegido.aceleracion*(this.cansancio/100)*MathUtils.acos(this.velocidad.angleRad()));
+		acel.set(this.elegido.aceleracion*(this.cansancio/100)*MathUtils.cos(this.angulo*this.DegToRad)*Gdx.graphics.getDeltaTime(), this.elegido.aceleracion*(this.cansancio/100)*MathUtils.sin(this.angulo*this.DegToRad)*Gdx.graphics.getDeltaTime());
 		this.velocidad.add(acel);
 		if(this.velocidad.x > this.elegido.velocidadmax) {
 			this.velocidad.x = this.elegido.velocidadmax;
@@ -34,7 +40,16 @@ public class Barco {
 	public void frenar(boolean natural) {
 		Vector2 acel = new Vector2();
 		if(natural) {
-			acel.set(this.frenadanatural*MathUtils.asin(this.velocidad.angleRad()), this.frenadanatural*MathUtils.acos(this.velocidad.angleRad()));
+			acel.set(this.frenadanatural*Math.abs(MathUtils.cos(this.angulo*this.DegToRad))*Gdx.graphics.getDeltaTime(), this.frenadanatural*Math.abs(MathUtils.sin(this.angulo*this.DegToRad))*Gdx.graphics.getDeltaTime());
+			if(this.velocidad.x < 0) {
+				acel.x = -acel.x;
+			}
+			if(this.velocidad.y < 0) {
+				acel.y = -acel.y;
+			}
+			this.velocidad.add(acel);
+		} else {
+			acel.set(-this.elegido.aceleracion*(this.cansancio/100)*MathUtils.cos(this.angulo*this.DegToRad)*Gdx.graphics.getDeltaTime(), -this.elegido.aceleracion*(this.cansancio/100)*MathUtils.sin(this.angulo*this.DegToRad)*Gdx.graphics.getDeltaTime());
 			this.velocidad.add(acel);
 			if(this.velocidad.x < -this.elegido.velocidadmax) {
 				this.velocidad.x = -this.elegido.velocidadmax;
@@ -42,15 +57,9 @@ public class Barco {
 			if(this.velocidad.y < -this.elegido.velocidadmax) {
 				this.velocidad.y = -this.elegido.velocidadmax;
 			}
-		} else {
-			acel.set(-this.elegido.aceleracion*(this.cansancio/100)*MathUtils.asin(this.velocidad.angleRad()), -this.elegido.aceleracion*(this.cansancio/100)*MathUtils.acos(this.velocidad.angleRad()));
-			this.velocidad.add(acel);
-			if(this.velocidad.x < this.elegido.velocidadmax) {
-				this.velocidad.x = -this.elegido.velocidadmax;
-			}
-			if(this.velocidad.y < this.elegido.velocidadmax) {
-				this.velocidad.y = -this.elegido.velocidadmax;
-			}
 		}
+	}
+	public void actualizarposicion() {
+		this.posicion.add(velocidad);
 	}
 }
--- Code/core/src/com/mygdx/code/Code.java
+++ Code/core/src/com/mygdx/code/Code.java
@@ -2,24 +2,51 @@ package com.mygdx.code;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.math.*;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Keys;
 
 public class Code extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
-	
+	Sprite barquito;
+	Barco boat;
+	TipoBarco elegido;
 	@Override
 	public void create () {
+		Vector2 pos = new Vector2(100,100);
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+		barquito = new Sprite(img,20,50);
+		elegido = new TipoBarco(10f,10f,"Neutro",10f,20f);
+		boat = new Barco(elegido,pos);
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
+		ScreenUtils.clear(1, 1, 1, 1);
+		if(Gdx.input.isKeyPressed(Keys.A)) {
+			boat.girarIzquierda();
+		}
+		if(Gdx.input.isKeyPressed(Keys.D)) {
+			boat.girarDerecha();
+		}
+		if(Gdx.input.isKeyPressed(Keys.W)) {
+			boat.acelerar();
+		}
+		if(Gdx.input.isKeyPressed(Keys.S)) {
+			boat.frenar(false);
+		} else {
+			boat.frenar(true);
+		}
+		boat.actualizarposicion();
+		barquito.setPosition(boat.posicion.x,boat.posicion.y);
+		barquito.setRotation(boat.angulo-90);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		barquito.draw(batch);
 		batch.end();
 	}
 	

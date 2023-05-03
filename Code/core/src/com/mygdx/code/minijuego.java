2b9b53b0c10bf62de24e34a76a4da0666f32e09e
MVictoriaHuesca <victoriahuesca@uma.es>
2023-05-03 18:31:50 +0200

    commit

--- Code/core/src/com/mygdx/code/minijuego.java
+++ Code/core/src/com/mygdx/code/minijuego.java
@@ -8,8 +8,7 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
-import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator.FreeTypeFontParameter;
+
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
@@ -18,7 +17,7 @@ public class minijuego implements Screen{
     final Code game;
     //private Stage stage;
 	private float anchoPantalla, altoPantalla;
-	private Texture fondo, pescador, canaPescar, barraProgreso, indicador, pez;
+	private Texture fondo, pescador, canaPescar, barraProgreso, indicador, pezV, pezH;
     private SpriteBatch batch;
     private float posY;
     private int puntuacion;
@@ -41,19 +40,13 @@ public class minijuego implements Screen{
         batch = new SpriteBatch();
 
         //puntuacion
-        FreeTypeFontGenerator generator = new FreeTypeFontGenerator(Gdx.files.internal("fuente.ttf"));
-        FreeTypeFontParameter parameter = new FreeTypeFontParameter();
-        parameter.size = 15;
-        fuente = generator.generateFont(parameter);
-
-        
+        fuente = new BitmapFont();
+        fuente.setColor(0, 0, 0, 1);
+        fuente.getData().setScale(5, 5);
+        pezH = new Texture("pezHorizontal.png");
 
         //fondo
         fondo = new Texture("island_pixel_art.png");
-       
-       /* Image imageFondo = new Image(fondo);
-        imageFondo.setSize(anchoPantalla, altoPantalla);
-        stage.addActor(imageFondo); */
 
         //pescador
         pescador = new Texture("pescador.png");
@@ -61,10 +54,6 @@ public class minijuego implements Screen{
         
         //caña
         canaPescar = new Texture("canapescar.png");
-        Image imageCanaPescar = new Image(canaPescar);
-       /*  imageCanaPescar.setSize(5, 10);
-        imageCanaPescar.setPosition(anchoPantalla/2 + 25, altoPantalla/2 + 25);
-        stage.addActor(imageCanaPescar); */
 
         //barra progreso
         barraProgreso = new Texture("barraProgresoVertical.png");
@@ -73,11 +62,15 @@ public class minijuego implements Screen{
         indicador = new Texture("indicadorVertical.png");
 
         //pez
-        pez = new Texture("pezVertical.png");
+        pezV = new Texture("pezVertical.png");
         Random rnd = new Random();
         float max = (altoPantalla/2 + 211);
         float min = (altoPantalla/2 - 22);
         posY = min + rnd.nextFloat() * (max - min);
+    }
+
+    private boolean pescado(){
+
     }
 
     @Override
@@ -93,22 +86,41 @@ public class minijuego implements Screen{
         batch.draw(indicador, anchoPantalla/2 + 130, altoPantalla/2 - 22, 30, 50);
 
         //dibujar pez en una posicion aleatoria de la barra
-        
-        batch.draw(pez, anchoPantalla/2 + 130, posY, 30, 50);
+        Boolean empezar = false; 
+        batch.draw(pezV, anchoPantalla/2 + 130, posY, 30, 50);
 
-        fuente.draw(batch, strPuntuacion,10, 10);
+        /* 
+        if(Gdx.input.touchDown()){
+           empezar = true;
+        }
+        
+        if(empezar){
+            Random rnd = new Random();
+            posY = posY + rnd.nextFloat() * ((altoPantalla/2 + 211) - posY)
+            batch.draw(pezV, anchoPantalla/2 + 130, posY, 30, 50)
+        }
+
+        */
+        
+        //dibujar puntuacion
+        fuente.draw(batch, strPuntuacion, 100, altoPantalla-70);
+        batch.draw(pezH, 230, altoPantalla-125, 80, 50);
+
+        /* 
+        if(pescado){
+            puntuacion++;
+            fuente.draw(batch, strPuntuacion, 100, altoPantalla-70);
+        } */
+        
 
         batch.end();
-        /*stage.act(delta);
-        stage.draw();*/
 
     }
 
     @Override
     public void resize(int width, int height) {
 
-    }
-
+    } 
     @Override
     public void pause() {
 

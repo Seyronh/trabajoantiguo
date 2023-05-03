9909eec119df46d00e40b5078fb58c63b5a78d77
MVictoriaHuesca <victoriahuesca@uma.es>
2023-05-03 14:29:37 +0200

    commit

--- Code/core/src/com/mygdx/code/minijuego.java
+++ Code/core/src/com/mygdx/code/minijuego.java
@@ -1,10 +1,15 @@
 package com.mygdx.code;
 
+import java.util.Random;
+
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator.FreeTypeFontParameter;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
@@ -13,24 +18,35 @@ public class minijuego implements Screen{
     final Code game;
     //private Stage stage;
 	private float anchoPantalla, altoPantalla;
-	private Texture fondo, pescador, canaPescar, barraProgreso, indicador;
+	private Texture fondo, pescador, canaPescar, barraProgreso, indicador, pez;
     private SpriteBatch batch;
+    private float posY;
     private int puntuacion;
+    private String strPuntuacion;
     private final int MAX_PUNTOS;
+    private BitmapFont fuente;
 
     public minijuego(final Code game){
         this.game = game;
         anchoPantalla = Gdx.graphics.getWidth();
 		altoPantalla = Gdx.graphics.getHeight();
         puntuacion = 0;
+        strPuntuacion = puntuacion + "/3";
         MAX_PUNTOS = 3;
     }
 
     @Override
     public void show() {
         //stage = new Stage();
-
         batch = new SpriteBatch();
+
+        //puntuacion
+        FreeTypeFontGenerator generator = new FreeTypeFontGenerator(Gdx.files.internal("fuente.ttf"));
+        FreeTypeFontParameter parameter = new FreeTypeFontParameter();
+        parameter.size = 15;
+        fuente = generator.generateFont(parameter);
+
+        
 
         //fondo
         fondo = new Texture("island_pixel_art.png");
@@ -55,6 +71,13 @@ public class minijuego implements Screen{
 
         //indicador
         indicador = new Texture("indicadorVertical.png");
+
+        //pez
+        pez = new Texture("pezVertical.png");
+        Random rnd = new Random();
+        float max = (altoPantalla/2 + 211);
+        float min = (altoPantalla/2 - 22);
+        posY = min + rnd.nextFloat() * (max - min);
     }
 
     @Override
@@ -62,13 +85,18 @@ public class minijuego implements Screen{
         
         batch.begin();
         batch.draw(fondo, 0, 0, anchoPantalla, altoPantalla);
-        batch.draw(pescador, anchoPantalla/2 + 220, altoPantalla/2 - 15, 100, 150);
+        batch.draw(pescador, anchoPantalla/2 + 225, altoPantalla/2 - 15, 90, 150);
         batch.draw(canaPescar,anchoPantalla/2 + 270, altoPantalla/2 - 15, 180, 150);
         batch.draw(barraProgreso, anchoPantalla/2 + 120, altoPantalla/2 - 30, 50, 300);
 
         //posicion inicial indicador
         batch.draw(indicador, anchoPantalla/2 + 130, altoPantalla/2 - 22, 30, 50);
 
+        //dibujar pez en una posicion aleatoria de la barra
+        
+        batch.draw(pez, anchoPantalla/2 + 130, posY, 30, 50);
+
+        fuente.draw(batch, strPuntuacion,10, 10);
 
         batch.end();
         /*stage.act(delta);
